b24c42d4c00235b8de8538193cc910f4c9cd8cc9
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-11-16 17:17:13 +0530

    refactor: rename folder from const_with_validation to const

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Appwrite.MixProject do
   def project do
     [
       app: :appwrite,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

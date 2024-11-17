726ea467628594cd283d89ad1cdef9929c851a37
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-11-17 18:41:35 +0530

    chore: Added types and consts modules group and added a installation introduction mix.exs

---
+++ guides/introduction/installation.md
@@ -0,0 +1,13 @@
+# Installation
+
+Appwrite is published on [Hex](https://hex.pm/packages/appwrite). Add it to your list of dependencies in
+`mix.exs`:
+
+```elixir
+# mix.exs
+def deps do
+  [
+    {:appwrite, "~> 0.1.2"}
+  ]
+end
+```
--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Appwrite.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/iamkanishka/appwrite"
+  @version "0.1.2"
+
   def project do
     [
       app: :appwrite,
-      version: "0.1.1",
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       # Package information for Hex
       description: "Appwrite package for elixir",
       package: package()
@@ -40,9 +44,114 @@ defmodule Appwrite.MixProject do
       # License, e.g., MIT, Apache 2.0
       licenses: ["MIT"],
       links: %{
-        "GitHub" => "https://github.com/iamkanishka/appwrite"
+        "GitHub" => @source_url
       },
       maintainers: ["Kanishka Naik"]
+    ]
+  end
+
+  defp docs do
+    # [
+    #   extras: [
+    #     "LICENSE.md": [title: "License"],
+    #     "README.md": [title: "Overview"],
+    #     "parameters.md": [],
+    #     "CHANGELOG.md": []
+    #   ],
+    #   main: "readme",
+    #   logo: "assets/plaid-logo.png",
+    #   source_url: @source_url,
+    #   source_ref: "master",
+    #   formatters: ["html"]
+    # ]
+    [
+      main: "appwrite",
+      # api_reference: false,
+      # logo: "assets/oban-logo.svg",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      extra_section: "GUIDES",
+      formatters: ["html"],
+      extras: extras(),
+      # groups_for_extras: groups_for_extras(),
+      groups_for_modules: groups_for_modules()
+      # skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp extras do
+    [
+      # Introduction
+      "guides/introduction/installation.md"
+    ]
+  end
+
+  defp groups_for_modules do
+    [
+      Consts: [
+       Appwrite.Consts.AuthenticationFactor,
+       Appwrite.Consts.AuthenticationType,
+       Appwrite.Consts.Browsers,
+       Appwrite.Consts.CreditCard,
+       Appwrite.Consts.ExecutionMethod,
+       Appwrite.Consts.Flag,
+       Appwrite.Consts.ImageFormat,
+       Appwrite.Consts.ImageGravity,
+       Appwrite.Consts.OAuthProvider
+
+      ],
+      Types: [
+       Appwrite.Types.AlgoArgon2,
+       Appwrite.Types.AlgoBcrypt,
+       Appwrite.Types.AlgoMd5,
+       Appwrite.Types.AlgoPhpass,
+       Appwrite.Types.AlgoScryptModified,
+       Appwrite.Types.AlgoSha,
+       Appwrite.Types.Continent,
+       Appwrite.Types.ContinentList,
+       Appwrite.Types.Country,
+       Appwrite.Types.CountryList,
+       Appwrite.Types.Currency,
+       Appwrite.Types.CurrencyList,
+       Appwrite.Types.Document,
+       Appwrite.Types.DocumentList,
+       Appwrite.Types.Execution,
+       Appwrite.Types.ExecutionList,
+       Appwrite.Types.File,
+       Appwrite.Types.FileList,
+       Appwrite.Types.Headers,
+       Appwrite.Types.Headers,
+       Appwrite.Types.Identity,
+       Appwrite.Types.IdentityList,
+       Appwrite.Types.Jwt,
+       Appwrite.Types.Language,
+       Appwrite.Types.LanguageList,
+       Appwrite.Types.LocaleCode,
+       Appwrite.Types.LocaleCodeList,
+       Appwrite.Types.Locale,
+       Appwrite.Types.Log,
+       Appwrite.Types.LogList,
+       Appwrite.Types.Membership,
+       Appwrite.Types.MembershipList,
+       Appwrite.Types.MfaChallenge,
+       Appwrite.Types.MfaFactors,
+       Appwrite.Types.MfaRecoveryCodes,
+       Appwrite.Types.MfaType,
+       Appwrite.Types.Phone,
+       Appwrite.Types.PhoneList,
+       Appwrite.Types.Preference,
+       Appwrite.Types.Session,
+       Appwrite.Types.SessionList,
+       Appwrite.Types.Target,
+       Appwrite.Types.Team,
+       Appwrite.Types.TeamList,
+       Appwrite.Types.Token,
+       Appwrite.Types.User
+
+
+
+      ],
+
     ]
   end
 end

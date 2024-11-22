827af4c131bc3456b087a1e160c8d6d45ee554b0
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-11-22 18:46:16 +0530

    feat: Added  function  get_secret, get_project_id, get_base_url for fetching secret, project_id and root_uri

--- lib/appwrite.ex
+++ lib/appwrite.ex
@@ -1,18 +1,35 @@
 defmodule Appwrite do
-  @moduledoc """
-  Documentation for `Appwrite`.
-  """
+  defmodule MissingProjectIdError do
+    defexception message: """
+                 The `project_id` is required for calls to Plaid. Please either configure `project_id`
+                 in your config.exs file or pass it into the function via the `config` argument.
 
-  @doc """
-  Hello world.
+                 config :appwrite, project_id: "your_project_id" (test)
+                 config :appwrite, project_id: "your_project_id" (development)
+                 config :appwrite, project_id: "your_project_id" (production)
+                 """
+  end
 
-  ## Examples
+  defmodule MissingSecretError do
+    defexception message: """
+                 The `secret` is required for calls to Plaid. Please either configure `secret`
+                 in your config.exs file or pass it into the function via the `config` argument.
 
-      iex> Appwrite.hello()
-      :world
+                 config :appwrite, secret: "your_secret" (test)
+                 config :appwrite, secret: "your_secret" (development)
+                 config :appwrite, secret: "your_secret" (production)
+                 """
+  end
 
-  """
-  def hello do
-    :world
+  defmodule MissingRootUriError do
+    defexception message: """
+                 The root_uri is required to specify the Plaid environment to which you are
+                 making calls, i.e. sandbox, development or production. Please configure root_uri in
+                 your config.exs file.
+
+                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (test)
+                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (development)
+                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (production)
+                 """
   end
 end

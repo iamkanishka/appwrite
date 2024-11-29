65c1eca2393347ccb9398a6f662407ee31ceba7c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-11-29 09:00:59 +0530

    chore: Updated the status in teams service

--- lib/appwrite/services/teams.ex
+++ lib/appwrite/services/teams.ex
@@ -4,8 +4,6 @@ defmodule Appwrite.Services.Teams do
   to enable them to share read and write access to your project resources, such as database documents or storage files.
   Each user who creates a team becomes the team owner and can delegate the ownership role by inviting a new team member.
   Only team owners can invite new users to their team.
-
-  Status: In Testing
   """
 
   @doc """

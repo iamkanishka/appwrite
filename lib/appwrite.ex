f28e07779f25ab77d9ba666ee90cb796f683b8ac
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2024-11-30 07:30:06 +0530

    chore: "Update Client module to use dynamic config and env vars for endpoint, project ID, and secret"

--- lib/appwrite.ex
+++ lib/appwrite.ex
@@ -1,35 +1,37 @@
 defmodule Appwrite do
+  alias Appwrite.Utils.Client
+
   defmodule MissingProjectIdError do
     defexception message: """
-                 The `project_id` is required for calls to Appwrite. Please either configure `project_id`
-                 in your config.exs file or pass it into the function via the `config` argument.
+                 The `project_id` is required for calls to Appwrite. Please either configure `appwrite_project_id`
+                 in your config.exs file.
 
-                 config :appwrite, project_id: "your_project_id" (test)
-                 config :appwrite, project_id: "your_project_id" (development)
-                 config :appwrite, project_id: "your_project_id" (production)
+                 config #{Client.get_app_name()}, appwrite_project_id: "your_project_id" (test)
+                 config #{Client.get_app_name()}, appwrite_project_id: "your_project_id" (development)
+                 config #{Client.get_app_name()}, appwrite_project_id: "your_project_id" (production)
                  """
   end
 
   defmodule MissingSecretError do
     defexception message: """
-                 The `secret` is required for calls to Appwrite. Please either configure `secret`
-                 in your config.exs file or pass it into the function via the `config` argument.
+                 The `secret` is required for calls to Appwrite. Please either configure `appwrite_secret`
+                 in your config.exs file.
 
-                 config :appwrite, secret: "your_secret" (test)
-                 config :appwrite, secret: "your_secret" (development)
-                 config :appwrite, secret: "your_secret" (production)
+                 config #{Client.get_app_name()}, appwrite_secret: "your_secret" (test)
+                 config #{Client.get_app_name()}, appwrite_secret: "your_secret" (development)
+                 config #{Client.get_app_name()}, appwrite_secret: "your_secret" (production)
                  """
   end
 
   defmodule MissingRootUriError do
     defexception message: """
                  The root_uri is required to specify the Appwrite environment to which you are
-                 making calls, i.e. sandbox, development or production. Please configure root_uri in
+                 making calls, i.e. sandbox, development or production. Please configure appwrite_root_uri in
                  your config.exs file.
 
-                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (test)
-                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (development)
-                 config :appwrite, root_uri:  "https://cloud.appwrite.io/v1" (production)
+                 config #{Client.get_app_name()}, appwrite_secret:  "https://cloud.appwrite.io/v1" (test)
+                 config #{Client.get_app_name()}, appwrite_secret:  "https://cloud.appwrite.io/v1" (development)
+                 config #{Client.get_app_name()}, appwrite_secret:  "https://cloud.appwrite.io/v1" (production)
                  """
   end
 end

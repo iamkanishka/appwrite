93a20292eb682819c132222340ac8b323d745348
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-22 12:21:10 +0530

    .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,10 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    # Allow macro-style calls without parens in tests
+    assert: 1,
+    assert: 2,
+    refute: 1,
+    refute: 2
+  ]
 ]

e89b224e83d3ff35205cc5e742dc25569d73c2e4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-22 12:24:37 +0530

    module Appwrite.Consts.Browser

---
+++ lib/appwrite/consts/browser.ex
@@ -0,0 +1,26 @@
+defmodule Appwrite.Consts.Browser do
+  @moduledoc """
+  Browser type codes used by the Appwrite Avatars service.
+
+  | Browser                | Code   |
+  |------------------------|--------|
+  | Avant Browser          | `"aa"` |
+  | Android WebView Beta   | `"an"` |
+  | Google Chrome          | `"ch"` |
+  | Google Chrome iOS      | `"ci"` |
+  | Google Chrome Mobile   | `"cm"` |
+  | Chromium               | `"cr"` |
+  | Mozilla Firefox        | `"ff"` |
+  | Safari                 | `"sf"` |
+  | Mobile Safari          | `"mf"` |
+  | Microsoft Edge         | `"ps"` |
+  | Microsoft Edge iOS     | `"oi"` |
+  | Opera Mini             | `"om"` |
+  | Opera                  | `"op"` |
+  | Opera Next             | `"on"` |
+  """
+
+  use Appwrite.Consts.Behaviour,
+    values: ~w(aa an ch ci cm cr ff sf mf ps oi om op on),
+    name: "browser"
+end

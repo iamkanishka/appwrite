3057ad40f1dcb8ad637eb9a454f2ce17989a63e4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-22 12:22:36 +0530

    Updated

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,11 @@
 import Config
 
-import_config "#{Mix.env()}.exs"
+# Default SDK configuration.
+# Override per-environment in config/dev.exs, config/test.exs, config/prod.exs.
+# In production applications, use config/runtime.exs with System.fetch_env!/1.
+config :appwrite,
+  project_id: System.get_env("APPWRITE_PROJECT_ID"),
+  secret: System.get_env("APPWRITE_SECRET"),
+  root_uri: System.get_env("APPWRITE_ENDPOINT", "https://cloud.appwrite.io/v1")
+
+import_config "#{config_env()}.exs"
